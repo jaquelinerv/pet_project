feae1d8a44c3401bdb63e8a5ed57a1a559f3ade2
Jaqueline <jaquelinevaz95@gmail.com>
2023-05-24 08:56:11 -0300

    Ajustes feitos

--- Pet Project/script.js
+++ Pet Project/script.js
@@ -1,64 +1,68 @@
-let characters = [{
+let characters = [
+  {
     name: "Lux",
     life: 100,
     mana: 100,
-    damage: 100,
+    damage: 500,
     armor: 50,
     mana_regen: 20,
-    image: "imgs/lux-lux.jpg"
-},
-{
+    image: "imgs/lux-lux.jpg",
+  },
+  {
     name: "Caitlyn",
     life: 100,
     mana: 100,
-    damage: 100,
+    damage: 200,
     armor: 50,
     mana_regen: 20,
-    image: "imgs/caitlyn-caitlyn.jpg"
-},
-{
+    image: "imgs/caitlyn-caitlyn.jpg",
+  },
+  {
     name: "Zed",
     life: 100,
     mana: 100,
-    damage: 100,
+    damage: 250,
     armor: 50,
     mana_regen: 20,
-    image: "imgs/zed-zed.jpg"
-},
-]
+    image: "imgs/zed-zed.jpg",
+  },
+];
 
 //passar por todos campeos coom loop
 const personagem = document.getElementById("personagem");
 const containerPersonagem = document.getElementById("container-personagens");
 
-characters.forEach( (campeao)=> {
+characters.forEach((campeao) => {
   const botao = document.createElement("button");
-  botao.setAttribute('id', campeao.name);
+  botao.setAttribute("id", campeao.name);
   botao.innerText = campeao.name;
-  botao.setAttribute('onclick', `showCharacter(${campeao.name})`);
+  botao.setAttribute("onclick", `showCharacter(${campeao.name})`);
 
   containerPersonagem.append(botao);
-  
-})
-
+});
 
 //função que adiciona a imagem do campeão na tela. falta adicionar o resto dos dados do campeão.
-function showCharacter(event){
-personagem.innerHTML = null; //funciona mas nao é o ideal =)
-const nomePersonagem = event.innerText;
-const personagemEncontrado = characters.find((campeao)=> {
-  return campeao.name == nomePersonagem;
-})
+function showCharacter(event) {
+  personagem.innerHTML = null; //funciona mas nao é o ideal =)
+  const nomePersonagem = event.innerText;
+  const personagemEncontrado = characters.find((campeao) => {
+    return campeao.name == nomePersonagem;
+  });
 
-const imagem = document.createElement("img")
-imagem.setAttribute('src', personagemEncontrado.image) 
-const danoCampeao = document.createElement('span');
-danoCampeao.innerText = personagemEncontrado.damage;
-personagem.append(danoCampeao)
-personagem.append(imagem);
+  const imagem = document.createElement("img");
+  imagem.setAttribute("src", personagemEncontrado.image);
 
-};
+  const danoCampeao = document.createElement("span");
+  const manaCampeao = document.createElement("span");
+  danoCampeao.innerText = `${
+    "Dano do campeão: " + personagemEncontrado.damage
+  }`;
+  manaCampeao.innerText = `${"Mana do campeão: " + personagemEncontrado.mana}`;
 
-
+  personagem.append(danoCampeao);
+  personagem.append(document.createElement("br"));
+  personagem.append(manaCampeao);
+  personagem.append(imagem);
+}
 
 //adiconar a imagem num container e apagar depois q clicou no proximo (limpar e add um novo)
